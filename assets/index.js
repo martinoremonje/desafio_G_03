18170eb2de9ec335bfe331165433402b84771a61
Martin Orellana <106769606+martinoremonje@users.noreply.github.com>
2024-03-12 20:24:55 -0300

    creación de tabla en javascript index.js
    se crea la tabla por javascript

--- assets/index.js
+++ assets/index.js
@@ -2,7 +2,9 @@ let btn = document.getElementById("btn");
 let texto = document.getElementById("text1");
 let texto1 = document.getElementById("text2");
 let texto2 = document.getElementById("text3");
-let h2 = document.querySelectorAll("h2")
+let textoTable1 = document.getElementById("textTable1");
+let textoTable2 = document.getElementById("textTable2");
+let textoTable3 = document.getElementById("textTable3");
 let tabla = "<h2 style='color: red'>Radiologia</h2><br>";  
 let tabla2 = "<h2 style='color: red'>Traumatologia</h2><br>";
 let tabla3 = "<h2 style='color: red'>Dental</h2><br>"; 
@@ -32,6 +34,41 @@ const mostrarUltimos = () =>{
     texto.innerHTML = tabla;
     texto1.innerHTML = tabla2;
     texto2.innerHTML = tabla3;
+};
+const mostrarTablas = () =>{
+    let textoTabla1 = "<tr><th>Hora</th><th>Especialista</th><th>Paciente</th><th>Rut</th><th>Prevision</th></tr>";
+    let textoTabla2 = "<tr><th>Hora</th><th>Especialista</th><th>Paciente</th><th>Rut</th><th>Prevision</th></tr>";
+    let textoTabla3 = "<tr><th>Hora</th><th>Especialista</th><th>Paciente</th><th>Rut</th><th>Prevision</th></tr>";
+    Radiologia.forEach(e=>{
+        textoTabla1 +=  `<tr>
+        <td>${e.hora}</td>
+        <td>${e.especialista}</td>
+        <td>${e.paciente}</td>
+        <td>${e.rut}</td>
+        <td>${e.prevision}</td>
+        </tr>`;
+       
+    }); textoTable1.innerHTML = textoTabla1;
+    Traumatologia.forEach(e=>{
+        textoTabla2 +=  `<tr>
+        <td>${e.hora}</td>
+        <td>${e.especialista}</td>
+        <td>${e.paciente}</td>
+        <td>${e.rut}</td>
+        <td>${e.prevision}</td>
+        </tr>`;
+       
+    }); textoTable2.innerHTML = textoTabla2;
+    Dental.forEach(e=>{
+        textoTabla3 +=  `<tr>
+        <td>${e.hora}</td>
+        <td>${e.especialista}</td>
+        <td>${e.paciente}</td>
+        <td>${e.rut}</td>
+        <td>${e.prevision}</td>
+        </tr>`;
+       
+    }); textoTable3.innerHTML = textoTabla3;
 }
 
 
@@ -111,41 +148,41 @@ const Traumatologia = [
         "prevision":"ISAPRE"}
 ];
 const Dental = [
-    {"hora": "8:30:00", 
-    "especialista": "ANDREA ZUÑIGA", 
-    "paciente":"MARCELA RETAMAL", 
-    "rut": "11123425-6", 
-    "prevision":"ISAPRE"}
+    { hora: "8:30:00", 
+    especialista: "ANDREA ZUÑIGA", 
+    paciente: "MARCELA RETAMAL", 
+    rut: "11123425-6", 
+    prevision: "ISAPRE"},
+
+    { hora: "11:00:00", 
+    especialista: "MARIA PIA ZAÑARTU", 
+    paciente: "ANGEL MUÑOZ", 
+    rut: "9878789-2", 
+    prevision: "ISAPRE"}
     ,
-    {"hora": "11:30:00", 
-    "especialista": "FEDERICO SUBERCASEAUX", 
-    "paciente":"PAMELA ESTRADA", 
-    "rut": "15345241-3", 
-    "prevision":"ISAPRE"}
+    { hora: "11:30:00", 
+    especialista: "SCARLETT WITTING", 
+    paciente: "MARIO KAST", 
+    rut: "7998789-5", 
+    prevision: "FONASA"}
     ,
-    {"hora": "15:00:00", 
-    "especialista": "FERNANDO WURTHZ",
-     "paciente":"ARMANDO LUNA", 
-     "rut": "16445345-9", 
-     "prevision":"ISAPRE"}
-     ,
-     {"hora": "15:30:00", 
-     "especialista": "ANA MARIA GODOY",
-      "paciente":"MANUEL GODOY",
-       "rut": "17666419-0", 
-       "prevision":"FONASA"}
-       ,
-       {"hora": "15:30:00", 
-       "especialista": "ANA MARIA GODOY",
-        "paciente":"MANUEL GODOY",
-         "rut": "17666419-0", 
-         "prevision":"FONASA"}
-       ,
-       {"hora": "14:00:00", 
-       "especialista": "RAQUEL VILLASECA", 
-       "paciente":"ANA SEPULVEDA", 
-       "rut": "14441281-0",
-        "prevision":"ISAPRE"}
-];
+    { hora: "13:00:00", 
+    especialista: "FRANCISCO VON TEUBER", 
+    paciente: "KARIN FERNANDEZ", 
+    rut: "18887662-K", 
+    prevision: "FONASA"}
+    ,
+    { hora: "13:30:00", 
+    especialista: "EDUARDO VIÑUELA", 
+    paciente: "HUGO SANCHEZ", 
+    rut: "17665461-4", 
+    prevision: "FONASA"}
+    ,
+    { hora: "14:00:00", 
+    especialista: "RAQUEL VILLASECA", 
+    paciente: "ANA SEPULVEDA", 
+    rut: "14441281-0", 
+    prevision: "ISAPRE"},
+  ];
 
 console.log(Radiologia, Traumatologia, Dental)
